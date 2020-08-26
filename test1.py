ef398f0b2a8593369600f54db3bd9a8169290cc9
manaskumarpradhan <manaspradhan.kumar@gmail.com>
2020-08-26 17:46:59 +0530

    first commit

---
+++ test1.py
@@ -0,0 +1,4 @@
+from test2 import Calc
+
+t = Calc(2, 3)
+print(t.add())
--- test2.py
+++ test2.py
@@ -1,1 +1,12 @@
-#changes
+# changes
+class Calc():
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+
+
